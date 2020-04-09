59aecb36720a03e0d3f1f32125b284babd6ef3c7
Tarun <hellknight@turing.teslanet>
2020-04-09 19:23:10 +0530

    Started Battle RPG tutorial

---
+++ Battle/main.py
@@ -0,0 +1,8 @@
+from classes.game import Person, bcolors
+
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 10, "dmg": 80},
+         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+
+player = Person(460, 65, 60, 34, magic)
+
