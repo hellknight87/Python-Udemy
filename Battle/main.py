f35c0510acf11fb006cad98d6323a8f2fad952bb
Tarun <hellknight@turing.teslanet>
2020-04-09 20:10:16 +0530

    Added Enemy Script

--- Battle/main.py
+++ Battle/main.py
@@ -1,8 +1,15 @@
 from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
+         {"name": "Thunder", "cost": 12, "dmg": 80},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
+
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
 
