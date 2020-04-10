d4648c7dac17091c14246640d806c2c281d29a73
Tarun <hellknight@turing.teslanet>
2020-04-10 19:52:29 +0530

    Skipping to next chapter

--- Battle/classes/game.py
+++ Battle/classes/game.py
@@ -1,5 +1,4 @@
 import random
-from .magic import Spell
 import pprint
 
 class bcolors:
@@ -13,7 +12,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic):
+    def __init__(self, hp, mp, atk, df, magic, items):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -22,7 +21,8 @@ class Person:
         self.atkh = atk + 10
         self.df = df
         self.magic = magic
-        self.actions = ["Attack", "Magic"]
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
@@ -56,15 +56,24 @@ class Person:
     
     def choose_action(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
+        print(bcolors.OKBLUE + bcolors.BOLD + "ACTIONS" + bcolors.ENDC)
         for item in self.actions:
-            print(str(i) + ":", item)
+            print("     " + str(i) + ".", item)
             i += 1
 
     def choose_magic(self):
         i = 1
        
-        print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
+        print(bcolors.OKBLUE + bcolors.BOLD + "MAGIC" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
+            print("     " + str(i) + ".", spell.name, "(cost:", str(spell.cost) + ")")
+            i += 1
+
+
+    def choose_item(self):
+        i = 1
+
+        print(bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        for item in self.items:
+            print("     " + str(i) + ".", item.name, ":", item.description, " (x5)")
             i += 1
--- Battle/main.py
+++ Battle/main.py
@@ -1,5 +1,6 @@
 from classes.game import Person, bcolors
 from classes.magic import Spell
+from classes.inventory import Item
 
 # Create black magic
 fire = Spell("Fire", 10, 100, "black")
@@ -12,9 +13,22 @@ quake = Spell("Quake", 14, 140, "black")
 cure = Spell("Cure", 12, 120, "white")
 cura = Spell("Cura", 18, 200, "white")
 
+
+# Create some Items
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
+superpotion = Item("Super-Potion", "potion", "Heals 500 HP", 500)
+elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
+hielixer = Item("Mega-Elixer", "elixer", "Fully restores HP/MP of all", 9999)
+
+grenade = Item("Grenade", "attack", "Deals 500 damage", 500)
+
+player_spells = [fire, thunder, blizzard, meteor, cure, cura]
+player_items = [potion, hipotion, superpotion, elixer, hielixer, grenade]
+
 #Instantiate People
-player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, cure, cura])
-enemy = Person(1200, 65, 45, 25, [])
+player = Person(460, 65, 60, 34, player_spells, player_items )
+enemy = Person(1200, 65, 45, 25, [], [])
 
 running = True
 i = 0
@@ -53,6 +67,10 @@ while running:
             print(bcolors.OKBLUE + "\n " + spell + " deals", str(magic_dmg), "points of damage" + bcolors.ENDC)
                 
         
+    elif index == 2:
+        player.choose_item()
+        item_choice = int(input("Choose item: ")) - 1
+
 
     enemy_choice = 1
 
